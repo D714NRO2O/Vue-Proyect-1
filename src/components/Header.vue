043d819a1f0c45d5a2a56f65811480531ff8bc41
D714NRO2O <dsrozot10@gmail.com>
2025-02-21 17:54:50 -0500

    cambios directorios

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <img alt="Logo Lingüi" class="logo" src="@/assets/logoLingui.png" width="280" height="125" />
+  </header>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <RouterLink to="/">Log In</RouterLink>
+    <RouterLink to="/user">Usuario</RouterLink>
+    <RouterLink to="/support">Soporte</RouterLink>
+    <RouterLink to="/settings">Configuración</RouterLink>
+    <RouterLink to="/reset-password">Restablecer Contraseña</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+nav a:first-of-type {
+  border: 0;
+}
+</style>
